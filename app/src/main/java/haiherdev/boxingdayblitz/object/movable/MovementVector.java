438584f56ca76ce3ffc4ef6e2d559ce0577a2d87
dhman27 <hospitald27@gmail.com>
2015-02-16 14:21:11 -0500

    MovementVectors contain a position where the object will move, and the time it will take to move there. every update, the object will move towards that point at constant pace, measured by the time since the last move. Once the object has fully moved, the MovementVector will destroy itself (remove from the list, Garbage Collector will pick it up)

---
+++ app/src/main/java/haiherdev/boxingdayblitz/object/movable/MovementVector.java
@@ -0,0 +1,71 @@
+package haiherdev.boxingdayblitz.object.movable;
+
+import haiherdev.boxingdayblitz.object.GameObject;
+import haiherdev.boxingdayblitz.object.vector.Vector2d;
+
+/**
+ * Created by David on 2/16/2015.
+ */
+public class MovementVector {
+
+    private Vector2d v2d;
+    private long startTime;
+    private long lastTime;
+    private final long END_TIME;
+
+    private Movable o;
+
+    private Vector2d scale;
+
+    public MovementVector (Movable o, Vector2d v2d, long time) {
+        this.v2d = v2d;
+        this.END_TIME = time;
+        this.startTime = System.nanoTime();
+        this.lastTime = startTime;
+
+        this.o = o;
+
+        this.scale = new Vector2d (
+            deltaX() / END_TIME,
+            deltaY() / END_TIME
+        );
+    }
+
+    public void updateTime () {
+        if (getDeltaStartTime() >= END_TIME) {
+            destroy ();
+        } else {
+
+        }
+
+        lastTime = System.nanoTime();
+    }
+
+    private void moveObject () {
+        long deltaTime = getDeltaLastTime();
+        double xScale = scale.getX() * deltaTime;
+        double yScale = scale.getY() * deltaTime;
+        o.move(new Vector2d (xScale, yScale));
+    }
+
+    private long getDeltaStartTime () {
+        return System.nanoTime() - startTime;
+    }
+
+    private long getDeltaLastTime () {
+        return System.nanoTime() - lastTime;
+    }
+
+    private double deltaX () {
+        return Math.abs (o.getX() - v2d.getX());
+    }
+
+    private double deltaY () {
+        return Math.abs (o.getY() - v2d.getY());
+    }
+
+    private void destroy () {
+        o.movements.remove(this);
+    }
+
+}
